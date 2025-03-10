c91a7bb24e3773c56628b7ac57726d36d5989892
istiak19 <istiak15-14128@diu.edu.bd>
2025-03-10 22:52:32 +0600

    all page update

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,9 +1,9 @@
 const SectionTitle = ({ subTitle, title, description }) => {
     return (
         <div className="text-center py-10 space-y-3">
-            <h3 className="text-xl text-red-500 font-bold">{subTitle}</h3>
-            <h1 className="text-5xl font-bold">{title}</h1>
-            <p className="text-gray-500">{description}</p>
+            <h3 className="text-2xl text-red-500 font-bold">{subTitle}</h3>
+            <h1 className="text-4xl font-bold">{title}</h1>
+            <p className="text-gray-600">{description}</p>
         </div>
     );
 };
